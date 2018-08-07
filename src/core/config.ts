cb9fb975b29387ed267063c9bbf1004f3e4292c4
ueqt <ueqtxu@gmail.com>
2018-08-07 17:24:04 +0800

    feat(examples): add examples and vue basic logic and debug settings

---
+++ src/core/config.ts
@@ -0,0 +1,9 @@
+/**
+ * 配置类
+ */
+export class Config {
+  /**
+   * 是否启用性能调试
+   */
+  performance!: boolean
+}
--- src/vuets.ts
+++ src/vuets.ts
@@ -1,4 +1,30 @@
+import { Config } from './core/config'
+
 /**
  * Vue
  */
-export default class Vue {}
+export default class Vue {
+  /**
+   * 配置
+   */
+  public static config: Config
+
+  /**
+   * 当前 View Model 的uid
+   */
+  private _uid = 0
+
+  /**
+   * 全局uid计数
+   */
+  private static uid = 0
+
+  constructor(options?: any) {
+    this.init(options)
+  }
+
+  private init(options?: any) {
+    const vm = this
+    vm._uid = Vue.uid++
+  }
+}
